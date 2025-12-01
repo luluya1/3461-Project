3bfe2f73a6cf4386d9c5682f70096b86296678b7
kaylayoung123 <kaylayoung123@gmail.com>
2025-12-01 13:57:20 -0500

    put in phedo code comments

--- Server.py
+++ Server.py
@@ -1,9 +1,23 @@
 from socket import *
+
+# 1. Create a TCP server socket and bind it to an IP address and port
 serverPort = 12000
 serverSocket = socket(AF_INET,SOCK_STREAM) #creating a server side socket
 serverSocket.bind(('',serverPort)) # bind() method associates a server socket with a specific address and port on the local machine
+
+# 2. Start listening for incoming client connections
 serverSocket.listen(1) #this line means server listens for the TCP connection req.
 print('The server is ready to receive') # printing to confirm that TCP server is up and ready
+
+# 3. Initialize an empty list to store connected clients
+connectedClients = []
+
+# 4. while server is running do
+    #Accept a new client connection
+    #Add the client to the list of active clients
+    #Start a new thread to handle communication with that client
+    #end while
+
 while True: #always welcoming
     connectionSocket, addr = serverSocket.accept() #When a client knocks on this door, the program invokes the method for serverSocket,
     #which creates a new socket in the server, called , dedicated to this particular client.
